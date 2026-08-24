bd182b9f3fd3fb6bc58d3a08ba889c4bc4a220d9
vstefan699-ai <vstefan699@gmail.com>
2026-08-24 09:49:43 +0200

    Add files via upload

---
+++ Lesson_2/messages.js
@@ -0,0 +1,16 @@
+//Listing 2.1
+"use strict"; // Enforces stricter parsing and error handling in JavaScript
+
+// Create an array of messages
+let messages = [
+    "A change of environment can be a good thing!", // Encouraging message about adaptability
+    "You will make it!",                            // Motivational message
+    "Just run with the code!"                       // Playful message encouraging action
+];
+
+// Loop through the messages and log each one
+messages.forEach(m => console.log(m));
+
+
+
+
